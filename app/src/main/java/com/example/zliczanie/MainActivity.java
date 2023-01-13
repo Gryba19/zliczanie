c540ea8455c22bf313dd2904c8f47e2a2f21fe02
4THP <gryba@zspswiecie,pl>
2023-01-13 09:04:40 +0100

    a

--- app/src/main/java/com/example/zliczanie/MainActivity.java
+++ app/src/main/java/com/example/zliczanie/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 int count=tekst.length();
                 String litery="abcdefghijklmnoprstuvwyz";
                 String znakispecjalne="!@#$%^&*";
+
                 pokaz.findViewById(R.id.count);
                 pokaz.setText("liczba znaków wynosi"+count);
 
